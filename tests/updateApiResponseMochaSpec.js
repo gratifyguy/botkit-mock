574103f72863963946e6bfe554572d6c4e8b8323
kn1cht <ito.turukasiwa@gmail.com>
2017-07-22 01:23:09 +0900

    Fix storage.process to return error when ok == false

--- tests/updateApiResponseMochaSpec.js
+++ tests/updateApiResponseMochaSpec.js
@@ -86,6 +86,40 @@ describe('change api data for action with extending', ()=>{
     });
 });
 
+describe('mock api error case', ()=>{
+    beforeEach((done)=>{
+        this.controller = Botmock({
+            debug: false,
+        });
+        this.bot = this.controller.spawn({type: 'slack'});
+
+        this.bot.api.setData('users.list', {ok: false, error: 'not_authed'});
+        this.bot.api.setData('channels.info', {channel1: {ok: false, error: 'channel_not_found'}});
+        done();
+    });
+
+    it('should return error in users.list call', (done)=>{
+        this.bot.api.callAPI('users.list', {}, (err, data)=>{
+            assert.equal(err, 'not_authed')
+            done();
+        })
+    });
+
+    it('should return error in channels.info call', (done)=>{
+        this.bot.api.callAPI('channels.info', {channel: 'channel1'}, (err, data)=>{
+            assert.equal(err, 'channel_not_found')
+            done();
+        })
+    });
+
+    it('should not return error when ok is not specified', (done)=>{
+        this.bot.api.setData('users.list', {members: [{id: '2'}]});
+        this.bot.api.callAPI('users.list', {}, (err, data)=>{
+            assert.equal(err, null)
+            done();
+        })
+    });
+});
 
 describe('api urls', ()=>{
     beforeEach(()=>{
