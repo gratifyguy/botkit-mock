c68826914d04f0ecadc9144b70ac0cc441bbae59
ihorrusinko <ihor.rusinko@gmail.com>
2018-04-05 15:12:19 +0300

    0.1.7

--- tests/extendSlackBotMochaSpec.js
+++ tests/extendSlackBotMochaSpec.js
@@ -9,7 +9,7 @@ describe('extendSlackBot', () => {
 				debug: false,
 			});
 
-			function botExtender (bot, botkit, config) {
+			function botExtender(bot, botkit, config) {
 				bot.customReply = function (message, text) {
 					bot.reply(message, 'Something new... ' + text);
 				};
@@ -27,29 +27,30 @@ describe('extendSlackBot', () => {
 			assert.equal(this.bot.detailed_answers[this.userMessage.channel][0].text, 'Something new... hello');
 		});
 	});
+
 	describe('support replyAndUpdate', () => {
-        beforeEach(() => {
-            this.controller = Botmock({
-                debug: false,
-            });
+		beforeEach(() => {
+			this.controller = Botmock({
+				debug: false,
+			});
 
-            this.bot = this.controller.spawn({type: 'slack'});
-        });
+			this.bot = this.controller.spawn({type: 'slack'});
+		});
 
-        it('should log the updated message to replies as well', () => {
-                this.userMessage = {
-                	user: 'some user id',
-					channel: 'some channel'
-				};
-                this.bot.replyAndUpdate(this.userMessage, 'hello', (err, src, updateResponse) => {
-                	updateResponse('changed', (err) => {
-                		if (err) fail(this, err);
-                    });
+		it('should log the updated message to replies as well', () => {
+			this.userMessage = {
+				user: 'some user id',
+				channel: 'some channel'
+			};
+			this.bot.replyAndUpdate(this.userMessage, 'hello', (err, src, updateResponse) => {
+				updateResponse('changed', (err) => {
+					if (err) fail(this, err);
+				});
 
-                    assert.equal(this.bot.detailed_answers[this.userMessage.channel][0].text, 'hello');
-                    assert.equal(this.bot.detailed_answers[this.userMessage.channel][1].text, 'changed');
-                });
-        });
+				assert.equal(this.bot.detailed_answers[this.userMessage.channel][0].text, 'hello');
+				assert.equal(this.bot.detailed_answers[this.userMessage.channel][1].text, 'changed');
+			});
+		});
 
-    });
+	});
 });
